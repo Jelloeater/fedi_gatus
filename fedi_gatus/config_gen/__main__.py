23ad0aaf42e8f20b30870f770a4f206e1b4adc0c
Jesse Schoepfer <jelloeater@gmail.com>
2023-07-08 17:25:31 -0400

    wip: File logging working, need to fix path next

--- fedi_gatus/config_gen/__main__.py
+++ fedi_gatus/config_gen/__main__.py
@@ -1,8 +1,14 @@
 import logging
 
+import fedi_gatus.config_gen.gen
+
 
 def main():
     logging.info("Config Gen Start")
+    top = fedi_gatus.config_gen.gen.generate_full_config()
+    # FIXME Log file to correct path for docker
+    with open("gatus-config.yaml", "w") as f:
+        f.write(str(top))
 
 
 if __name__ == "__main__":
