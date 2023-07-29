de8a7d11eb159ca8d9da9a24567c097cdc245ea4
Jesse Schoepfer <jelloeater@gmail.com>
2023-07-29 17:48:45 -0400

    wip: Need to fix API router for docs

--- fedi_gatus/api_server/__main__.py
+++ fedi_gatus/api_server/__main__.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from starlette import status
 from starlette.responses import RedirectResponse
 
@@ -14,14 +14,15 @@ else:
 
 class web_app:
     def __init__(self):
-        self.app = FastAPI()
+        BASE_URL = os.getenv("API_BASE_URL")
+        self.app = FastAPI(debug=True, docs_url=f'/{BASE_URL}/docs')
 
-        @self.app.get("/", include_in_schema=False)
-        async def root():
-            return RedirectResponse(self.app.docs_url)
+        # @self.app.get(f'/{BASE_URL}', include_in_schema=False)
+        # async def root():
+        #     return RedirectResponse(self.app.docs_url)
 
         @self.app.get(
-            "/healthcheck",  # For AutoDocs
+            f'/{BASE_URL}/healthcheck',  # For AutoDocs
             responses={
                 status.HTTP_200_OK: {"description": "Connection to DB OK"},
                 status.HTTP_503_SERVICE_UNAVAILABLE: {
@@ -50,7 +51,9 @@ class Server:
     @classmethod
     def start_server(cls):  # pragma: no cover # TODO Remove when test fixed
         u = uvicorn
-        c = u.config.Config(app=web_app().app, host=cls.local_nic(), port=cls.port, proxy_headers=True)
+        c = u.config.Config(app=web_app().app, host=cls.local_nic(), port=cls.port, proxy_headers=True,
+                            # root_path="/api/v1"
+                            )
         w = u.Server(c)
         w.run()
 
