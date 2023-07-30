6d1ea2d4cc99661c038fc723a1ed399190a805d6
Jesse Schoepfer <jelloeater@gmail.com>
2023-07-29 22:59:57 -0400

    Added template to security ignore

--- fedi_gatus/api_server/__main__.py
+++ fedi_gatus/api_server/__main__.py
@@ -16,12 +16,12 @@ class web_app:
     def __init__(self):
         self.app = FastAPI(debug=True)
 
-        @self.app.get('/', include_in_schema=False)
+        @self.app.get("/", include_in_schema=False)
         async def root():
             return RedirectResponse(self.app.docs_url)
 
         @self.app.get(
-            f'/healthcheck',  # For AutoDocs
+            "/healthcheck",  # For AutoDocs
             responses={
                 status.HTTP_200_OK: {"description": "Connection to DB OK"},
                 status.HTTP_503_SERVICE_UNAVAILABLE: {
@@ -50,9 +50,13 @@ class Server:
     @classmethod
     def start_server(cls):  # pragma: no cover # TODO Remove when test fixed
         u = uvicorn
-        c = u.config.Config(app=web_app().app, host=cls.local_nic(), port=cls.port, proxy_headers=True,
-                            # root_path="/api/v1"
-                            )
+        c = u.config.Config(
+            app=web_app().app,
+            host=cls.local_nic(),
+            port=cls.port,
+            proxy_headers=True,
+            # root_path="/api/v1"
+        )
         w = u.Server(c)
         w.run()
 
